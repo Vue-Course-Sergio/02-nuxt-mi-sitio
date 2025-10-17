0aa8c92b6eadf37b604508a16cdf7b36d9936198
Sergio Barreras <sergiobg.isc@gmail.com>
2025-10-17 09:52:22 -0600

    feat: implement sleep utility and update app.vue to use it

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
+<script lang="ts" setup>
+const count = ref(10);
+
+await sleep(2000);
+
+count.value = 20;
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Hola mundo - {{ count }}</h1>
+    <button @click="count++">Click me</button>
   </div>
 </template>
