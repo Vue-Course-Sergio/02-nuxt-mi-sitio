75d5f60dd7ab898a2a46ae459f70454ee739f4af
Sergio Barreras <sergiobg.isc@gmail.com>
2025-10-20 11:47:13 -0600

    feat: add custom error page for handling 404 errors with improved styling and functionality

--- app/pages/products.vue
+++ app/pages/products.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+throw createError({
+  statusCode: 404,
+  statusMessage: "Page not found!!",
+  data: {
+    myCustomField: true,
+    otherField: "Some value",
+  },
+});
+</script>
 
 <template>
-  <div>
-    Page: products
-  </div>
+  <div>Page: products</div>
 </template>
 
 <style scoped></style>
