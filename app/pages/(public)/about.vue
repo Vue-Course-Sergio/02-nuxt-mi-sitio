83d55a2689a9f6e1794759a23c938ad7a624e8c2
Sergio Barreras <sergiobg.isc@gmail.com>
2025-10-21 21:07:40 -0600

    Refactor code structure for improved readability and maintainability

--- app/pages/(public)/about.vue
+++ app/pages/(public)/about.vue
@@ -1,3 +1,19 @@
+<script lang="ts" setup>
+useSeoMeta({
+  title: "About | Nuxt App",
+  description: "This is the about page of our Nuxt application.",
+  ogTitle: "About | Nuxt App",
+  ogDescription: "This is the about page of our Nuxt application.",
+  ogType: "website",
+  ogUrl: "https://nuxt-app.com/about",
+  ogImage: "/og-image.png",
+  twitterCard: "summary_large_image",
+  twitterTitle: "About | Nuxt App",
+  twitterDescription: "This is the about page of our Nuxt application.",
+  twitterImage: "/og-image.png",
+});
+</script>
+
 <template>
   <div>
     <h1>About Page</h1>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
 
   css: ["~/assets/css/main.css"],
 
+  app: {
+    head: {
+      title: "Mi Sitio con Nuxt 4",
+      meta: [
+        {
+          name: "description",
+          content: "Este es mi sitio web construido con Nuxt 4",
+        },
+      ],
+    },
+  },
+
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
 
   // ssr: false,
