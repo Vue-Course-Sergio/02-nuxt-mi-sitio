5a7989d74c71db697870a9de587038721f401bb1
Sergio Barreras <sergiobg.isc@gmail.com>
2025-12-31 12:56:02 -0600

    feat: add Product model and seed data for various cloud services and security solutions

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
-import { PrismaClient } from "./generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { prisma } from "../server/utils/db";
+import { PrismaClient } from "./generated/client";
 import { siteReviews } from "./seed/site-reviews";
+import { products } from "./seed/products";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 const prisma = new PrismaClient({ adapter });
@@ -11,6 +13,10 @@ async function seed() {
 
   await prisma.siteReview.createMany({
     data: siteReviews,
+  });
+
+  await prisma.product.createMany({
+    data: products,
   });
 }
 
