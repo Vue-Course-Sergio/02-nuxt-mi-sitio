a755cdd6e648798d2e969abd84d733ff6958ac72
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-01 14:53:58 -0600

    feat: add admin role middleware to protect admin routes

--- server/api/admin/product/[id].ts
+++ server/api/admin/product/[id].ts
@@ -1,18 +1,6 @@
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, "id");
 
-  const session = await requireUserSession(event);
-
-  const hasAdminRole = session.user.roles.includes("admin");
-
-  if (!hasAdminRole) {
-    throw createError({
-      statusCode: 401,
-      statusMessage: "Unauthorized",
-      message: "You do not have permission to access this resource.",
-    });
-  }
-
   const product = await prisma.product.findUnique({
     where: { id: Number(id) },
   });
