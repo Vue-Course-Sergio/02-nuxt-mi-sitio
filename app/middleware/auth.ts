279867b9e117372fbe555ca3b4576dce62683961
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-01 13:25:00 -0600

    feat: add authentication middleware to protect dashboard routes

---
+++ app/middleware/auth.ts
@@ -0,0 +1,9 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const { isLoggedIn, isAdmin } = useAuthentication();
+
+  if (!isLoggedIn.value) return navigateTo("/login");
+
+  if (to.path.startsWith("/dashboard") && !isAdmin.value) {
+    return navigateTo("/");
+  }
+});
