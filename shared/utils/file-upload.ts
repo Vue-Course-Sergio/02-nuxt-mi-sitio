1eec1dc7cadf3eda0a37dd2b1128fd1d3b3ee263
Sergio Barreras <sergiobg.isc@gmail.com>
2026-01-02 12:33:58 -0600

    feat: add UUID support for file uploads and implement upload logic with Cloudinary

--- shared/utils/file-upload.ts
+++ shared/utils/file-upload.ts
@@ -1,3 +1,57 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, type UploadApiResponse } from "cloudinary";
+import { v4 as uuidv4 } from "uuid";
 
-export const fileUpload = (fileBuffer: Buffer<ArrayBufferLike>) => {};
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+interface UploadResponse {
+  success: boolean;
+  result: UploadApiResponse | undefined;
+}
+
+export const fileUpload = async (fileBuffer: Buffer<ArrayBufferLike>) => {
+  try {
+    const uuidFileName = uuidv4();
+
+    const uploadResult = await new Promise<UploadResponse>(
+      async (resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream(
+            {
+              resource_type: "image",
+              public_id: uuidFileName,
+              folder: "nuxt-course",
+            },
+            (err, result) => {
+              if (err) {
+                console.log({ err });
+                return reject({ success: false, error: err });
+              }
+              return resolve({ success: true, result });
+            }
+          )
+          .end(fileBuffer);
+      }
+    );
+
+    console.log({ result: uploadResult });
+
+    if (!uploadResult.success) throw new Error("Upload failed");
+
+    const optimizedUrl = cloudinary.url(uuidFileName, {
+      fetch_format: "auto",
+      quality: "auto",
+      width: 600,
+      height: 400,
+      crop: "limit",
+    });
+
+    return optimizedUrl;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error uploading file");
+  }
+};
